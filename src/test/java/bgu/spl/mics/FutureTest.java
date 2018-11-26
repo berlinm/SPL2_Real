857e5b052c256c860ba1a9818ef30630c3e089e3
תם <@TOM-LAP>
2018-11-26 19:39:55 +0200

    future test

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -2,29 +2,42 @@ package bgu.spl.mics;
 
 import org.junit.*;
 
+import java.lang.reflect.Array;
+
 import static org.junit.Assert.*;
 
 public class FutureTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
     public void get() {
 
+        Future<String> stringFuture=new Future<String>();
+        stringFuture.resolve("Tom");
+        Assert.assertEquals("Tom",stringFuture.get());
+        stringFuture.resolve("Berlin");
+        Assert.assertEquals("Berlin",stringFuture.get());
+
+        Future<Integer> IntegerFuture=new Future<Integer>();
+        IntegerFuture.resolve(5);
+        Assert.assertEquals(5,IntegerFuture.get().intValue());
     }
 
     @Test
     public void resolve() {
+        Future<Boolean> booleanFuture=new Future<Boolean>();
+        booleanFuture.resolve(true);
+        Assert.assertNotNull(booleanFuture.get());
+        Assert.assertEquals(true,booleanFuture.get());
+        booleanFuture.resolve(false);
+        Assert.assertEquals(false,booleanFuture.get());
     }
 
     @Test
     public void isDone() {
+        Future<String> stringFuture=new Future<String>();
+        Assert.assertEquals(false,stringFuture.isDone());
+        stringFuture.resolve("Resolve");
+        Assert.assertEquals(true,stringFuture.isDone());
     }
 
     @Test
