97e14662b878d7f7faa632d945ccd8d4a73d9ab3
tomger2 <tomger@post.bgu.ac.il>
2018-12-12 18:12:36 +0200

    Basic Classes for jason use

--- src/main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ src/main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -1,10 +1,184 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+
+import bgu.spl.mics.application.passiveObjects.BookInventoryInfo;
+import bgu.spl.mics.application.passiveObjects.Customer;
+import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
+import bgu.spl.mics.application.passiveObjects.ResourcesHolder;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output serialized objects.
  */
 public class BookStoreRunner {
+
+
     public static void main(String[] args) {
+
+
+        class JsonParser {
+
+            private InitialInventory initialInventory;
+            private InitialResources initialResources;
+            private Services services;
+
+            public JsonParser(InitialInventory initialInventory, InitialResources initialResources, Services services) {
+                this.initialInventory = initialInventory;
+                this.initialResources = initialResources;
+                this.services = services;
+            }
+
+            public JsonParser() {
+
+            }
+
+
+            class InitialInventory {
+                private Book[] bookInventoryInfos;
+
+                public InitialInventory(Book[] bookInventoryInfos) {
+                    for (int i = 0; i < bookInventoryInfos.length; i++) {
+                        this.bookInventoryInfos[i] = bookInventoryInfos[i];
+                    }
+                }
+
+            }
+
+            class Book {
+                private String bookTitle;
+                private int amount;
+                private int price;
+
+                public Book(String bookTitle, int amount, int price) {
+
+                    this.amount = amount;
+                    this.price = price;
+                    this.bookTitle = bookTitle;
+
+                }
+            }
+
+
+            class InitialResources {
+                private Vehicle[][] deliveryVehicles;
+
+                public InitialResources(Vehicle[][] deliveryVehicles) {
+
+                    for (int i = 0; i < deliveryVehicles.length; i++) {
+                        for (int j = 0; j < deliveryVehicles[i].length; j++) {
+                            this.deliveryVehicles[i][j] = deliveryVehicles[i][j];
+                        }
+                    }
+                }
+
+            }
+
+            class Vehicle {
+                private int license;
+                private int speed;
+
+                public Vehicle(int license, int speed) {
+                    this.license = license;
+                    this.speed = speed;
+                }
+            }
+
+            class Services {
+
+                private Time time;
+                private int selling;
+                private int inventoryService;
+                private int logistic;
+                private int resourceService;
+                private Customers customers;
+
+                public Services(Time time, int selling, int inventoryService, int logistic, int resourceService, Customers customers) {
+                    this.time = time;
+                    this.selling = selling;
+                    this.inventoryService = inventoryService;
+                    this.logistic = logistic;
+                    this.resourceService = resourceService;
+                    this.customers = customers;
+                }
+
+
+            }
+
+            class Time {
+                private int speed;
+                private int duration;
+
+                public Time(int speed, int duration) {
+                    this.speed = speed;
+                    this.duration = duration;
+                }
+
+            }
+
+            class Customers {
+
+                Customer[] customers;
+
+                public Customers(Customer[] customers) {
+                    this.customers = customers;
+                }
+
+            }
+
+            class Customer {
+
+                private int id;
+                private String name;
+                private String address;
+                private int distance;
+                private CreditCard creditCard;
+                private OrderSchedule orderSchedule;
+
+                public Customer(int id, String name, String address, int distance, CreditCard creditCard, OrderSchedule orderSchedule) {
+                    this.id = id;
+                    this.name = name;
+                    this.address = address;
+                    this.distance = distance;
+                    this.creditCard = creditCard;
+                    this.orderSchedule = orderSchedule;
+                }
+            }
+
+            class CreditCard {
+                private int number;
+                private int amount;
+
+                public CreditCard(int number, int amount) {
+                    this.amount = amount;
+                    this.number = number;
+                }
+
+            }
+
+            class OrderSchedule {
+                private BookByTick[] bookByTicks;
+
+                public OrderSchedule(BookByTick[] bookByTicks){
+                    this.bookByTicks=bookByTicks;
+                }
+
+            }
+
+
+            class BookByTick {
+                 private String bookTitle;
+                 private int tick;
+
+                 public BookByTick(String bookTitle, int tick) {
+                    this.bookTitle = bookTitle;
+                    this.tick = tick;
+                   }
+             }
+    }
+
+        JsonParser jsonParser=new JsonParser();
+
+
+
     }
 }
